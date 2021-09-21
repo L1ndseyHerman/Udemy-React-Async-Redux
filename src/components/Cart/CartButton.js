85ef572ac4dabc094229c45ae9ddf44a67308103
L1ndseyHerman <lherman21@jcu.edu>
2021-09-21 16:11:59 -0400

    Caught up to vid251, more Redux review.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 //  uiActions is an extra export, not the default one, so need it in brackets like this.
 import { uiActions } from '../../store/ui-slice';
@@ -7,6 +7,7 @@ import classes from './CartButton.module.css';
 const CartButton = (props) => {
 
   const dispatch = useDispatch();
+  const cartQuantity = useSelector(state => state.cart.totalQuantity); 
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
@@ -15,7 +16,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
