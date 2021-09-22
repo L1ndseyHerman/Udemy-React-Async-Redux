24b364d94c21798dc77773e507bbd203af56d56c
L1ndseyHerman <lherman21@jcu.edu>
2021-09-21 20:11:47 -0400

    Done with Section 19, although I'm not sure when the cart number stopped working....

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,13 @@ export const fetchCartData = () => {
 
         try {
             const cartData = await fetchData();
-            dispatch(cartActions.replaceCart(cartData));
+            dispatch(
+                cartActions.replaceCart({
+                    //  This will return an empty array if cartData.items is undefined!
+                    items: cartData.items || [],
+                    totalQuantity: cartData.totalQuantity
+                })
+            );
         } catch(error) {
             dispatch(
                 uiActions.showNotification({
@@ -47,7 +53,10 @@ export const sendCartData = (cart) => {
             const response = await fetch('https://udemy-react-async-redux-default-rtdb.firebaseio.com/cart.json', 
                 {
                     method: 'PUT',
-                    body: JSON.stringify(cart)
+                    body: JSON.stringify({
+                        items: cart.items, 
+                        totalQuantity: cart.totalQuantity
+                    })
                 }
             );
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -5,7 +5,8 @@ const cartSlice = createSlice({
     initialState: {
         items: [],
         totalQuantity: 0,
-        totalAmount: 0
+        totalAmount: 0, 
+        changed: false
     },
     reducers: {
         replaceCart(state, action) {
@@ -17,6 +18,7 @@ const cartSlice = createSlice({
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
             state.totalQuantity++;
+            state.changed = true;
             if (!existingItem) {
                 //  It's only ok to manipulate an object like this in Redux Toolkit, not reg Redux!
                 state.items.push({
@@ -35,6 +37,7 @@ const cartSlice = createSlice({
             const id = action.payload;
             const existingItem = state.items.find(item => item.id === id);
             state.totalQuantity--;
+            state.changed = true;
             if (existingItem.quantity === 1) {
                 state.items = state.items.filter(item => item.id !== id);
             } else {
